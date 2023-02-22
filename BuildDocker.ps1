11237919527adb9b30212b9af670804fa065b123
Titus <flew2bits@gmail.com>
2023-02-22 18:51:37 -0500

    support for docker and easy deploy

---
+++ BuildDocker.ps1
@@ -0,0 +1,12 @@
+﻿$AppName = Split-Path -Path (Get-Location) -Leaf
+$LowerAppName = $AppName.ToLower().Trim().Replace(" ", "");
+
+docker build -f $AppName\Dockerfile -t flew2bits/${LowerAppName}:latest .
+
+mkdir c:\temp\$LowerAppName\postgres -f
+
+dotnet dev-certs https -ep c:\temp\$LowerAppName\localhost.pfx -p SECURE
+
+cd $AppName
+docker compose up -d
+cd ..
--- RenameSolution.ps1
+++ RenameSolution.ps1
@@ -1,7 +1,13 @@
 ﻿$AppName = Split-Path -Path (Get-Location) -Leaf
+$AppNameSquished = $AppName.Trim().Replace(" ", "")
+$LowerAppName = $AppNameSquished.ToLower()
 
 Rename-Item Shell.sln "${AppName}.sln"
 Rename-Item Shell\Shell.csproj "${AppName}.csproj"
 Rename-Item Shell $AppName
 
-Get-ChildItem -Recurse -Include "*.cs","*.sln","*.csproj","*.cshtml" | ForEach-Object { $name = $_.FullName; (Get-Content $name) | ForEach-Object { $_ -replace "Shell","${AppName}" } | Set-Content $name }
+Get-ChildItem -Recurse -Include "*.sln","*.csproj","Dockerfile" | ForEach-Object { $name = $_.FullName; (Get-Content $name) | ForEach-Object { $_ -replace "Shell","${AppName}" } | Set-Content $name }
+Get-ChildItem -Recurse -Include "*.cs","*.cshtml" | ForEach-Object { $name = $_.FullName; (Get-Content $name) | ForEach-Object { $_ -replace "Shell","${AppNameSquished}" } | Set-Content $name }
+Rename-Item $AppName\docker-compose.yml docker-compose.yml.bak
+Get-Content ${AppName}\docker-compose.yml.bak | ForEach-Object { $_ -replace "shell",$LowerAppName} | Set-Content $AppName\docker-compose.yml
+Remove-Item $AppName\docker-compose.yml.bak
